cae0ead171a101f832f86f5282c4ca7a16a4ba6c
Vadym Kobzar <votvad@gmail.com>
2022-07-22 00:39:45 +0300

    finish2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const ingredientEl = {
+    ingredientsList: document.querySelector("#ingredients"),
+};
+const newIngredientList = ingredients.map(nameIngredients => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("item");
+    listItem.textContent = nameIngredients;
+    return listItem;
+});
+
+ingredientEl.ingredientsList.append(...newIngredientList);
